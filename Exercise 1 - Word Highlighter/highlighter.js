62b85d4f00a1d25e7ff279a831f7eef1608f4af2
gasangw <thomasgasangwa@gmail.com>
2023-07-24 09:45:48 +0200

    Solving first question

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,21 @@
-console.log("Exercise 1 - Word Highlighter");
+// console.log("Exercise 1 - Word Highlighter");
+
+const myText = document.getElementById('myParagraph').textContent;
+
+let text = myText.split(' ')
+
+let count = 0;
+let countedWords = {}
+
+text.map((ele) => {
+  countedWords[ele] = (countedWords[ele] || 0) +1;
+    if(countedWords[ele] === 2) count++
+})
+console.log(countedWords)
+
+console.log(typeof(myText))
+
+
+
+
+
